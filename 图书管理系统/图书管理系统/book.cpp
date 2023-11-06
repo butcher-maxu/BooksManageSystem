425fbad925711136edc92ab83f32641a5df1b218
maerhshi <3133169343@qq.com>
2023-11-06 21:35:14 +0800

    删除冗余文件及目录

--- 图书管理系统/图书管理系统/book.cpp
+++ 图书管理系统/图书管理系统/book.cpp
@@ -1,114 +1,109 @@
 #include "book.h"
-book::book()//构造函数
-	{ 
-	    bookName="未赋值";
-	    bookISBN=NULLID;
-	}
 
-string book::getName()const//获得书名
+book::book() // 构造函数
 {
-	return bookName;
+    bookName = "未赋值";
+    bookISBN = NULLID;
 }
 
-string book::getbookAuthor()const//获得作者
-	{
-		return bookAuthor;
-    }
+string book::getName() const // 获得书名
+{
+    return bookName;
+}
 
-string book::getbookISBN()const//获得ISBN
-	{
-		return bookISBN;
-	}
+string book::getbookAuthor() const // 获得作者
+{
+    return bookAuthor;
+}
 
-string book::gettype()const//获得类型
-	{
-		return type;
-	}
+string book::getbookISBN() const // 获得ISBN
+{
+    return bookISBN;
+}
 
-string book::getbookPress()const//获得出版社
-	{
-		return bookPress;
-	}
+string book::gettype() const // 获得类型
+{
+    return type;
+}
 
-int book::getbookStatue()const//获得状态
-	{
-		return bookStatue;
-	}
+string book::getPublisher() const // 获得出版社
+{
+    return publisher;
+}
 
-void book::setName(string a)//设定书名
-	{
-		bookName=a;
-	}
+bool book::getbookStatue() const // 获得状态
+{
+    return bookStatus;
+}
 
-void book::setAuthor(string b)//设定作者
-	{
-		bookAuthor=b;
-	}
+void book::setName(string bookname) // 设定书名
+{
+    bookName = bookname;
+}
 
-void book::setISBN(string c)//设定ISBN码
-	{
-		bookISBN=c;
-    }
+void book::setAuthor(string bookauthor) // 设定作者
+{
+    bookAuthor = bookauthor;
+}
 
-void book::settype(string d)//设定书项
-	{
-		type=d;
-	}
+void book::setISBN(string isbn) // 设定ISBN码
+{
+    bookISBN = isbn;
+}
 
-void book::setPress(string e)//设定出版社
-	{
-		bookPress=e;
-	}
+void book::settype(string type) // 设定书项
+{
+    type = type;
+}
 
-void book::setStatue(int f=0)//设定状态
-	{
-		bookStatue=f;
-	}
-	
-void book::readBookInformation()//读入书籍信息函数
-	{
-    cout<<"请输入书名："<<endl;//读入书名
-	cin>>bookName;
-	
-    cout<<"请输入作者名："<<endl;//读入作者名
-    cin>>bookAuthor;
+void book::setPress(string publisher) // 设定出版社
+{
+    publisher = publisher;
+}
 
-	cout<<"请输入ISBN码："<<endl;//读入ISBN码
-	cin>>bookISBN;
+void book::setStatue(int bookstatus = 0) // 设定状态
+{
+    bookStatus = bookstatus;
+}
 
-	cout<<"请输入类别："<<endl;//读入类别
-	cin>>type;
+void book::readBookInformation() // 读入书籍信息函数
+{
+    cout << "请输入书名：" << endl; // 读入书名
+    cin >> bookName;
 
-	cout<<"请输入出版社："<<endl;//读入出版社
-	cin>>bookPress;
+    cout << "请输入作者名：" << endl; // 读入作者名
+    cin >> bookAuthor;
 
-	cout<<"请输入状态：(0代表在馆，1代表借出)"<<endl;//读入状态
-	cin>>bookStatue;
-	}
+    cout << "请输入ISBN码: " << endl; // 读入ISBN码
+    cin >> bookISBN;
 
-void book::printBookInformation()//输出书籍信息函数
-	{
-		cout<<"书名："<<bookName<<endl;//输出书名
-		cout<<"作者："<<bookAuthor<<endl;//输出作者
-		cout<<"ISBN码："<<bookISBN<<endl;//输出ISBN码
-		cout<<"类别："<<type<<endl;//输出类别
-		cout<<"出版社："<<bookPress<<endl;//输出出版社
-		changeStatue();//调用状态改变函数
-	}
+    cout << "请输入类别：" << endl; // 读入类别
+    cin >> type;
 
-void book::changeStatue()//判断状态
-	{
-	if(bookStatue==0)
-		{
-			cout<<"状态：在馆"<<endl;//输出状态
-		}
-		else
-		{
-			cout<<"状态：借出"<<endl;//输出状态
-		}
-		
-	}
+    cout << "请输入出版社：" << endl; // 读入出版社
+    cin >> publisher;
 
-book::~book()
-	{
-    }
+    cout << "请输入状态：(0代表在馆, 1代表借出)" << endl; // 读入状态
+    cin >> bookStatus;
+}
+
+void book::printBookInformation() // 输出书籍信息函数
+{
+    cout << "书名：" << bookName << endl;    // 输出书名
+    cout << "作者：" << bookAuthor << endl;  // 输出作者
+    cout << "ISBN码：" << bookISBN << endl;  // 输出ISBN码
+    cout << "类别：" << type << endl;        // 输出类别
+    cout << "出版社：" << publisher << endl; // 输出出版社
+    changeStatue();                          // 调用状态改变函数
+}
+
+void book::changeStatue() // 判断状态
+{
+    bookStatus = getbookStatue();
+    if (!bookStatus)
+        bookStatus = 1;
+    else
+        bookStatus = 0;
+}
+
+book::~book() {}
